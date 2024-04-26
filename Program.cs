b32a82dcadf26496fa9fc6b96fc66e6e262fffa3
Øystein Kallset <oysteinkallset@gmail.com>
2024-04-26 14:20:26 +0200

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int length = 10;
+int zeroIndex = 0;
+int firstIndex = 1;
+int currentNumber;
+
+Console.WriteLine(zeroIndex); 
+Console.WriteLine(firstIndex);   
+for (int i = 0; i < length; i++)
+{
+currentNumber = firstIndex + zeroIndex;
+zeroIndex = firstIndex;
+firstIndex = currentNumber;
+ Console.WriteLine(currentNumber); 
+}
+
